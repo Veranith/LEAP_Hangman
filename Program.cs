573c92214302f6f270f5882e60e12d01d83d6ea9
Ryan Robinson <274507+Veranith@users.noreply.github.com>
2021-09-14 10:50:40 -0600

    removes unneeded comments

--- Program.cs
+++ Program.cs
@@ -6,36 +6,8 @@ namespace Hangman
     {
         static void Main(string[] args)
         {
-            //requirements: welcome screen, 
-
-            // display class
-            // _ _ _ _ _ S _    Guess left: 6   Guessed letters:  f g t r 
-             
             Game game = new();
-
             game.start();
-
-            //UserIODisplay.welcomeMessage();
-            //UserIODisplay.printGameBoard(game.correctGuesses, game.word, game.remainingGuesses, game.failedGuesses);
-
-            //char test = UserIOInput.getLetterFromUser();
-            //if (game.word.Contains(test))
-            //{
-            //    game.correctGuesses.Add(test);
-            //}
-
-            //Console.WriteLine(test);
-            
-            // game class
-            //   -- array guessedLetters
-            //   -- int remaingLimbs
-            //   -- method didIwin()
-
-            // user IO class - would use display class
-
-            // Word class - 
-            //   -- randomWord()
-            //   -- dictionary
         }
     }
 }
